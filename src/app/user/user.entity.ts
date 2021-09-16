08f67b1f90ca008f1fd742901ca24743fac9a7ec
Mahdi Rashidi <m8rashidi@gmail.com>
2021-09-16 17:55:57 +0300

    feat: define user entity

---
+++ src/app/user/user.entity.ts
@@ -0,0 +1,14 @@
+import { Entity } from 'typeorm';
+import { IUser } from '../../models/user/user.model';
+
+@Entity('users')
+export class User implements IUser {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date;
+  firstName: string;
+  lastName: string;
+  email: string;
+  title: string;
+}
