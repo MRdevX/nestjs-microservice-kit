b902f7462e7ac835efad37d1c9c87ad9b5be8480
Mahdi Rashidi <m8rashidi@gmail.com>
2021-09-16 19:13:04 +0300

    feat: update config

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
 import { AppModule } from './app/app.module';
+import config from '@root/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const environment = config.get('app.env');
+  const logger = app.get(Logger);
 
   app.use(helmet());
   app.enableCors({ origin: true, credentials: true });
@@ -15,6 +18,7 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
+      disableErrorMessages: environment === 'production',
       transform: true,
       transformOptions: {
         enableImplicitConversion: true,
@@ -22,16 +26,22 @@ async function bootstrap() {
     }),
   );
 
-  const options = new DocumentBuilder()
-    .setTitle(process.env.npm_package_name)
-    .setVersion(process.env.npm_package_version)
-    .setDescription(process.env.npm_package_description)
-    .addBearerAuth()
-    .build();
+  if (environment !== 'production') {
+    const options = new DocumentBuilder()
+      .setTitle(process.env.npm_package_name)
+      .setVersion(process.env.npm_package_version)
+      .setDescription(process.env.npm_package_description)
+      .addBearerAuth()
+      .build();
 
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api/v1/docs', app, document);
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api/v1/docs', app, document);
+  }
 
-  await app.listen(4040);
+  await app.listen(config.get('app.port'), config.get('app.host'), () => {
+    logger.verbose(`Server started on ${config.get('app.host')}:${config.get('app.port')}, env: ${environment}`, 'App');
+    logger.verbose(`Connected to db "${config.get('db.name')}"`, 'Database');
+    logger.verbose(JSON.stringify(config.get('app.cors').map((v) => v.toString())), 'CORS');
+  });
 }
 process.nextTick(bootstrap);
