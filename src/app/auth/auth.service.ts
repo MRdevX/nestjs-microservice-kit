6b3aa2a1642e9074e035d52e19463586f8144535
Mahdi Rashidi <m8rashidi@gmail.com>
2021-09-27 18:49:05 +0300

    refactor: remove auth module from ms-kit

--- src/app/auth/auth.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { UserService } from '../user/user.service';
-
-@Injectable()
-export class AuthService {
-  constructor(public readonly userService: UserService) {}
-}
--- src/app/auth/jwt-payload.interface.ts
+++
@@ -1,7 +0,0 @@
-import { TokenType } from "../../models/token/token-type.enum";
-
-export interface JwtPayload {
-  sub: string;
-  email: string;
-  type: TokenType;
-}
