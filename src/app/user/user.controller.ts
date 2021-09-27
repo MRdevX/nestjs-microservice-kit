f4edcb3f594ee44629006a0868638f40e8ec3d41
Mahdi Rashidi <m8rashidi@gmail.com>
2021-09-27 22:46:51 +0300

    feat: update user controller

--- src/app/user/user.controller.ts
+++ src/app/user/user.controller.ts
@@ -1,4 +1,7 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post, Put } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
+import { UpdateResult } from 'typeorm';
+import { CreateUserDto, UpdateUserDto, UserDto } from '../../models/user/user.dto';
 import { CrudController } from '../core/crud/crud.controller';
 import { User } from './user.entity';
 import { UserService } from './user.service';
@@ -7,5 +10,18 @@ import { UserService } from './user.service';
 export class UserController extends CrudController<User> {
   constructor(private readonly userService: UserService) {
     super(userService);
+  }
+
+  @Post()
+  @ApiOperation({ summary: 'Create an User' })
+  async create(@Body() userData: CreateUserDto) {
+    const createdUser = await this.userService.create(userData);
+    return createdUser;
+  }
+
+  @Put(':id')
+  @ApiOperation({ summary: 'Update an User' })
+  async update(@Param('id') id: string, @Body() userData: UpdateUserDto): Promise<UpdateResult> {
+    return this.userService.update(id, userData);
   }
 }
--- src/models/user/user.dto.ts
+++ src/models/user/user.dto.ts
@@ -1,21 +1,47 @@
-import { IsString } from 'class-validator';
+import { IsEmail, IsString } from 'class-validator';
 import { Exclude } from 'class-transformer';
 import { IUser } from './user.model';
+import { ApiProperty, OmitType, PartialType, PickType } from '@nestjs/swagger';
 
 export class UserDto implements IUser {
+  id?: string;
+
+  @ApiProperty({
+    description: 'User First Name.',
+    example: 'Ralph',
+  })
   @IsString()
   firstName: string;
 
+  @ApiProperty({
+    description: 'User Last Name.',
+    example: 'Remington',
+  })
   @IsString()
   lastName: string;
 
-  @IsString()
+  @ApiProperty({
+    description: 'User Email Address.',
+    example: 'ralphremington@gmail.com',
+  })
+  @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'User Password',
+  })
   @IsString()
   @Exclude()
   password: string;
 
+  @ApiProperty({
+    description: 'User Title.',
+    example: 'Backend Engineer',
+  })
   @IsString()
   title: string;
 }
+
+export class CreateUserDto extends PickType(UserDto, ['email', 'password'] as const) {}
+
+export class UpdateUserDto extends PartialType(OmitType(UserDto, ['id', 'password', 'email'] as const)) {}
